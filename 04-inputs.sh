8bf708ca48da91e9a64f53084cb50bd243a007a9
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-17 23:04:10 +0530

    inputs

--- 04-inputs.sh
+++ 04-inputs.sh
@@ -1,2 +1,10 @@
-echo $1
-echo $2
+# 0-n , $*/$@, $# -> These are special variables and cannot be declared
+echo First Argument $1 # first Argument
+echo Second ARgument $2 # Second Argument
+
+echo Script Name $0 # Script Name
+
+echo All Arguments $*
+echo All Arguments $@ # All Arguments
+
+echo number of Arguments $# # Number of Arguments
