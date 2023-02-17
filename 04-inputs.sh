cd123c21483088f0c0a0ce5bb559ba90ca576aba
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-17 23:05:22 +0530

    inputs

--- 04-inputs.sh
+++ 04-inputs.sh
@@ -4,7 +4,7 @@ echo Second ARgument $2 # Second Argument
 
 echo Script Name $0 # Script Name
 
-echo All Arguments $*
-echo All Arguments $@ # All Arguments
+echo All Arguments = $*
+echo All Arguments = $@ # All Arguments
 
-echo number of Arguments $# # Number of Arguments
+echo number of Arguments = $# # Number of Arguments
