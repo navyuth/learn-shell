7c893e26b4ccbe18aa60407af577c104deee3db9
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-21 12:10:07 +0530

    conditions.sh

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -1,10 +1,12 @@
 number=$1
 string=$2
 
-if [ ${number} -eq 5 ]; then # then can go in the next line as well, also semi colon is also fine
+if [ "${number}" -eq 5 ]; then # then can go in the next line as well, also semi colon is also fine
   echo number is 5
 fi
 
-if [ "${string}" == "abc" ]; then
+if [ "${string}" == abc ]; then
   echo string is abc
 fi
+
+# It is always a good practice to quote the variable in expression
