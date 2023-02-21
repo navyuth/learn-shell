12aaf33038e2a5df1737b5613e9694fc824ecc02
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-21 12:06:10 +0530

    conditions.sh

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -1,7 +1,7 @@
 number=$1
 string=$2
 
-if [ "${number}" -eq 5 ]; then # then can go in the next line as well, also semi colon is also fine
+if [ ${number} -eq 5 ]; then # then can go in the next line as well, also semi colon is also fine
   echo number is 5
 fi
 
