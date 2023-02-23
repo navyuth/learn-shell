740107084dd77d5fd8ade972c5c0ab0c008a3a65
Navyuth <34467822+navyuth@users.noreply.github.com>
2023-02-23 11:01:58 +0530

    09-loops.sh

--- 08-case.sh
+++ 08-case.sh
@@ -19,3 +19,16 @@ case $name in
   *) echo No Traiings are available
     ;;
 esac
+
+#  case can compare only with the strings
+
+# Same in the if condition
+if [ "${name}" == "DevOps" ]; then
+    echo Welcome to the Training
+    echo Training starts at 7AM
+elif [ "${name}" == "AWS" ]; then
+      echo Welcome to the Training
+      echo Training starts at 6AM
+else
+  echo No other Trainings are available
+fi
---
+++ 09-loops.sh
@@ -0,0 +1,7 @@
+# Loops based on expression
+i=10
+
+while [ $i -gt 0 ]; do
+  echo $1
+  i=$(i-1)
+done
